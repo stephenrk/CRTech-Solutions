d57efecfae470d0ae664d684cb5a1e08255ce356
Stephen Knupfer <stephenrknupfer@hotmail.com>
2016-10-09 05:02:28 -0300

    Email form

--- sendemail.php
+++ sendemail.php
@@ -1,21 +1,18 @@
 <?php
-	header('Content-type: application/json');
-	$status = array(
-		'type'=>'success',
-		'message'=>'Email enviado!'
-	);
+$name       = @trim(stripslashes($_POST['name'])); 
+$from       = @trim(stripslashes($_POST['email'])); 
+$subject    = @trim(stripslashes($_POST['subject'])); 
+$message    = @trim(stripslashes($_POST['message'])); 
+$to   		= 'administrador@crtechsolutions.com.br';//replace with your email
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+$headers   = array();
+$headers[] = "MIME-Version: 1.0";
+$headers[] = "Content-type: text/plain; charset=utf-8";
+$headers[] = "From: {$name} <{$from}>";
+$headers[] = "Reply-To: <{$from}>";
+$headers[] = "Subject: {$subject}";
+$headers[] = "X-Mailer: PHP/".phpversion();
 
-    $email_from = $email;
-    $email_to = 'administrador@crtechsolutions.com.br';
+mail($to, $subject, $message, $headers);
 
-    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
-
-    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
-
-    echo json_encode($status);
-    die;
+die;
