ce371158d1575aee6bdd4eb225f8bd7a592900c4
Stephen Knupfer <stephenrknupfer@hotmail.com>
2016-10-09 04:51:10 -0300

    ...

--- sendemail.php
+++ sendemail.php
@@ -11,7 +11,7 @@
     $message = @trim(stripslashes($_POST['message'])); 
 
     $email_from = $email;
-    $email_to = 'stephenrknupfer@hotmail.com';
+    $email_to = 'administrador@crtechsolutions.com.br';
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
